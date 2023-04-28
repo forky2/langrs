85a1ba12708bd31d75fc6276143cf0d192430277
forky2 <63731115+forky2@users.noreply.github.com>
2023-04-28 14:58:13 +0100

    Tidied main. Fixed drop() for List. Started generic version of list.

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -3,16 +3,10 @@ use lists::first::List;
 fn main() {
     println!("Hello, world!");
 
-    let s = 5;
-    let s_ptr = &s;
-    println!("{:p}", s_ptr);
-
-    // for n in 1i64..100000000000000 {
-        let mut list = List::new();
-        let list_ptr = &list;
-        println!("{:p}", list_ptr);
-        for m in 1..1_000_000 {
-            list.push(m);
-        }
-    // }
+    let mut list = List::new();
+    let list_ptr = &list;
+    println!("{:p}", list_ptr);
+    for m in 1..1_000_000 {
+        list.push(m);
+    }
 }
